e14940e724c82dad4d5d68bec3da4b9e2f6cc647
Danny Stoll <stolld@cs.uni-freiburg.de>
2021-05-09 04:22:13 +0200

    rework script construction

--- hydra_plugins/script_launcher/script_launcher.py
+++ hydra_plugins/script_launcher/script_launcher.py
@@ -18,34 +18,12 @@ from omegaconf import DictConfig
 log = logging.getLogger(__name__)
 
 
-_batch_script_header_default = """#!/bin/bash
-#SBATCH --time ${hydra.launcher.time}
-#SBATCH --cpus-per-task ${hydra.launcher.cpus_per_task}
-#SBATCH --gres=gpus:${hydra.launcher.gpus}
-#SBATCH --mem-per-cpu=${hydra.launcher.mem_per_cpu}
-#SBATCH --job-name ${hydra.launcher.job_name}
-#SBATCH --output ${hydra.launcher.output}
-#SBATCH --error ${hydra.launcher.error}
-"""
-
-
 @dataclass
 class LauncherConfig:
     _target_: str = (
         "hydra_plugins.script_launcher.script_launcher.ScriptLauncher"
     )
-    batch_script_header: str = _batch_script_header_default
-    execute_command: Optional[str] = None
-    pre_execute_command: Optional[str] = None
-    time: str = "0-10:00"
-    cpus_per_task: int = 1
-    gpus: int = 0
-    mem_per_cpu: int = 4000
-    job_name: str = "default"
-    partition: str = "???"
-    max_parallel_jobs: Optional[int] = None
-    output: str = "default-output-error.oe"
-    error: str = "default-output-error.oe"
+    batch_script_template: str = "???"
 
 
 ConfigStore.instance().store(
@@ -54,29 +32,18 @@ ConfigStore.instance().store(
 
 
 class ScriptLauncher(Launcher):
-    def __init__(self, batch_script_header: str, execute_command: Optional[str], pre_execute_command: Optional[str],
-                 partition: str, max_parallel_jobs: Optional[int], output: Optional[str], error: Optional[str],
-                 **kwargs) -> None:
+    def __init__(self, batch_script_template: str, **kwargs) -> None:
         self.config: Optional[DictConfig] = None
         self.config_loader: Optional[ConfigLoader] = None
         self.task_function: Optional[TaskFunction] = None
 
-        # Coming from the the plugin's configuration
-        self.execute_command = execute_command
-        self.batch_script_header = batch_script_header
-        self.pre_execute_command = pre_execute_command
-        self.partition = partition
-        self.max_parallel_jobs = max_parallel_jobs
-
-        # Make sure cluster output/error folder exists
-        Path(output).parent.mkdir(parents=True, exist_ok=True)
-        Path(error).parent.mkdir(parents=True, exist_ok=True)
+        self.batch_script_template = batch_script_template
 
     def setup(
-        self,
-        config: DictConfig,
-        config_loader: ConfigLoader,
-        task_function: TaskFunction,
+            self,
+            config: DictConfig,
+            config_loader: ConfigLoader,
+            task_function: TaskFunction,
     ) -> None:
         self.config = config
 
@@ -93,26 +60,22 @@ class ScriptLauncher(Launcher):
         sweep_dir.mkdir(parents=True, exist_ok=True)
         log.info(f"Sweep output dir : {sweep_dir}")
 
+        # Make sure cluster output/error folder exists
+
         # Add task array flag to header
-        if self.max_parallel_jobs is None:
-            array_flag = f"#SBATCH --array 0-{len(job_overrides) - 1}\n"
-        else:
-            array_flag = f"#SBATCH --array 0-{len(job_overrides) - 1}%{self.max_parallel_jobs}\n"
-        batch_script_header = self.batch_script_header + array_flag
 
-        # Construct the switch case over different tasks
-        batch_script_task_switch = "case ${SLURM_ARRAY_TASK_ID} in\n"
-        for idx, overrides in enumerate(job_overrides):
-            idx = initial_job_idx + idx
-            overrides_string = " ".join(filter_overrides(overrides))
-            if overrides_string:
-                overrides_string += " "
-            overrides_string += f"hydra.job.id=$SLURM_JOB_ID hydra.job.num={idx}"
-            batch_script_task_switch += f"  {idx})\n    {self.execute_command} {overrides_string}\n    ;;\n"
-        batch_script_task_switch += "esac\n"
+        # Construct args list
+        args_list = "ARGS=(\n"
+        for overrides in job_overrides:
+            args_list += " ".join(filter_overrides(overrides)) + "\n"
+        args_list += ")"
 
-        # Put together the batch script from its parts and write to sweep dir
-        batch_script = "\n".join([batch_script_header, self.pre_execute_command, batch_script_task_switch])
+        # Construct batch script
+        batch_script = self.batch_script_template
+        batch_script = batch_script.replace("<PUT_ARGS>", args_list)
+        batch_script = batch_script.replace("<PUT_NUM_ARGS>", str(len(job_overrides)))
+
+        # Write batch script
         num_previous_batch_scripts = len(list(sweep_dir.glob("batch_script_*.sh")))
         batch_script_path = sweep_dir / f"batch_script_{num_previous_batch_scripts}.sh"
         batch_script_path.write_text(batch_script)
@@ -120,6 +83,6 @@ class ScriptLauncher(Launcher):
         # Submit the batch script
         log.info(f"ScriptLauncher is submitting {len(job_overrides)} jobs")
         try:
-            subprocess.run(["sbatch", f"-p {self.partition}", batch_script_path, "--pty bash"])
+            subprocess.run(["sbatch", batch_script_path])
         except FileNotFoundError:
             raise EnvironmentError("The sbatch command could not be found.")
